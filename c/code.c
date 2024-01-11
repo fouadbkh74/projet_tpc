f3b9d379b6795498efaa3d8231e151837094d1eb
fouadbkh74 <fouadbkh74@gmail.com>
2024-01-12 00:02:49 +0100

    initialisation du projet

---
+++ c/code.c
@@ -0,0 +1,17 @@
+#include "raylib.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+
+
+int main(){
+
+    printf("hello fouad raouf projet ");
+
+
+    return 0;
+}
