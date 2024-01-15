6fb20397fe395c8e622ef8cffadb1abf149bf3e4
fouadbkh74 <fouadbkh74@gmail.com>
2024-01-15 09:50:51 +0100

    gere

--- c/code.c
+++ c/code.c
@@ -1,45 +1,65 @@
 #define RAYGUI_IMPLEMENTATION
-
 #include "raylib.h"
 #include <raygui.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main(void) {
+    const int windowWidth = 1000;
+    const int windowHeight = 800;
 
 
 
+    InitWindow(windowWidth, windowHeight, "2D Button Raylib");
+int key = GetCharPressed();
+    Rectangle buttonBounds1 = { 30, 50, 120, 50 };
+    Rectangle buttonBounds2 = { 30, 130, 120, 50 };
+    Rectangle buttonBounds3 = { 30, 700, 120, 50 };
 
-int main(){
+        Rectangle textBox = {windowWidth / 2 - 200, 700, 100, 50};
+ bool mouseOnText = false;
+    bool editMode = false;
+    char inputText[3] = "\0"; // Pour un entier décima
 
-    const int l = 1000;
-    const int h = 800;
-Rectangle rec={200,200,100,100};
-
-    InitWindow(l,h,"test");
-
-    while(!WindowShouldClose()){
-
+    while (!WindowShouldClose()) {
         ClearBackground(RAYWHITE);
+if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
+            mouseOnText = true;
+
+            // Activer le mode d'édition si la souris est pressée
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                editMode = !editMode;
+            }
+        } else {
+            mouseOnText = false;
+        }
+
+
+
+
 
         BeginDrawing();
+             DrawText("TRIE PAR COMPTAGE", windowWidth/2-200, 10, 40, BLUE);
 
-          DrawRectangle(200,200,200,100,BLUE);
-          GuiButton(rec,"click");
+        // Draw the border rectangle
+        DrawRectangleLinesEx((Rectangle){ 10, 10, 160, 770 }, 4, BLUE);
+
+
+        // Draw the buttons
+        GuiButton(buttonBounds1, "trie");
+        GuiButton(buttonBounds2, "entre");
+        GuiButton(buttonBounds3, "exit");
 
 
 
         EndDrawing();
-
     }
-
 
     CloseWindow();
 
-
-
-
-
-
-
-return 0 ;}
+    return 0;
+}
