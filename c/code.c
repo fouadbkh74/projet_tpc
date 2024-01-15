7a84f56ac9aa88a3fee120bf804dc266cf32748b
fouadbkh74 <fouadbkh74@gmail.com>
2024-01-15 11:06:52 +0100

    fin d'intreface graphique

--- c/code.c
+++ c/code.c
@@ -1,60 +1,177 @@
-#define RAYGUI_IMPLEMENTATION
 #include "raylib.h"
-#include <raygui.h>
-#include <time.h>
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
+#define MAX_INPUT_CHARS1 2
+#define NUMBERS_COUNT1 8
+
+void DrawButtonWithText(Rectangle bounds, const char *text, int fontSize, bool isMouseOnButton) {
+    DrawRectangleRec(bounds, isMouseOnButton ? DARKGRAY : LIGHTGRAY);
+    DrawRectangleLinesEx((Rectangle){bounds.x, bounds.y, bounds.width, bounds.height}, 2, BLUE);
+
+    // Calculer la position du texte pour le centrer dans le bouton
+    Vector2 textSize = MeasureTextEx(GetFontDefault(), text, fontSize, 2);
+    Vector2 textPosition = {bounds.x + bounds.width / 2 - textSize.x / 2, bounds.y + bounds.height / 2 - textSize.y / 2};
+
+    DrawTextEx(GetFontDefault(), text, textPosition, fontSize, 2, BLUE);
+}
 
 int main(void) {
     const int windowWidth = 1000;
     const int windowHeight = 800;
 
+    InitWindow(windowWidth, windowHeight, "projet tp c");
 
+    Rectangle buttonBounds1 = {30, 50, 120, 50};
+    Rectangle buttonBounds2 = {30, 130, 120, 50};
+    Rectangle buttonBounds3 = {30, 700, 120, 50};
 
-    InitWindow(windowWidth, windowHeight, "2D Button Raylib");
-int key = GetCharPressed();
-    Rectangle buttonBounds1 = { 30, 50, 120, 50 };
-    Rectangle buttonBounds2 = { 30, 130, 120, 50 };
-    Rectangle buttonBounds3 = { 30, 700, 120, 50 };
-
-        Rectangle textBox = {windowWidth / 2 - 200, 700, 100, 50};
- bool mouseOnText = false;
+    Rectangle rectangles[10];
+    Rectangle textBox = {windowWidth / 2 - 200, 700, 100, 50};
+    bool mouseOnText = false;
     bool editMode = false;
-    char inputText[3] = "\0"; // Pour un entier décima
+    char inputText[3] = "\0"; // Pour un entier décimal
+    int i;
+
+    ///////////////////////////////////////
+    Rectangle textBox1 = {windowWidth / 2 - 10, 700, 400, 50};
+    bool mouseOnText1 = false;
+    bool editMode1 = false;
+    char inputText1[MAX_INPUT_CHARS1 + 1] = "\0"; // +1 for the null terminator
+
+    int numbers1[NUMBERS_COUNT1];  // Tableau pour stocker les nombres saisis
+    int numbersCount1 = 0;
+    int cpt1 = 0; // Variable pour compter le nombre de saisies
 
     while (!WindowShouldClose()) {
-        ClearBackground(RAYWHITE);
-if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
-            mouseOnText = true;
+        ClearBackground(WHITE);
 
-            // Activer le mode d'édition si la souris est pressée
+        // Gestion de la première boîte de texte
+        if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
+            mouseOnText = true;
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
                 editMode = !editMode;
+                editMode1 = false; // Désactiver le mode d'édition de la deuxième boîte de texte
             }
         } else {
             mouseOnText = false;
         }
 
+        // Gestion de la deuxième boîte de texte
+        if (CheckCollisionPointRec(GetMousePosition(), textBox1)) {
+            mouseOnText1 = true;
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                editMode1 = !editMode1;
+                editMode = false; // Désactiver le mode d'édition de la première boîte de texte
+            }
+        } else {
+            mouseOnText1 = false;
+        }
 
+        // Gestion de la première boîte de texte
+        if (editMode) {
+            int key = GetKeyPressed();
+            if (key > 0) {
+                if (isdigit(key) && strlen(inputText) < 2) {
+                    strcat(inputText, (char[]){(char)key, '\0'});
+                }
+            }
+        }
 
+        if (IsKeyPressed(KEY_ENTER) && strlen(inputText) > 0) {
+            int inputValue = atoi(inputText);
+            if (inputValue >= 0 && inputValue <= 99) {
+                printf("Valeur saisie : %d\n", inputValue);
 
+            } else {
+                printf("Erreur : La valeur doit être entre 0 et 99 inclus.\n");
+            }
+            inputText[0] = '\0';
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE) && strlen(inputText) > 0) {
+            inputText[strlen(inputText) - 1] = '\0';
+        }
+
+        // Gestion de la deuxième boîte de texte
+        if (editMode1) {
+            int key1 = GetKeyPressed();
+            if (key1 > 0) {
+                if (isdigit(key1) && strlen(inputText1) < MAX_INPUT_CHARS1) {
+                   strcat(inputText1, (char[]){(char)key1, '\0'});
+                } else if (key1 == ',') {
+                    int inputValue1 = atoi(inputText1);
+                    numbers1[numbersCount1++] = inputValue1;
+                    cpt1++;
+                    if (numbersCount1 >= NUMBERS_COUNT1) {
+                        numbersCount1 = 0;
+                    }
+                    inputText1[0] = '\0';
+                }
+            }
+        }
+
+        if (IsKeyPressed(KEY_ENTER) && strlen(inputText1) > 0) {
+            int inputValue1 = atoi(inputText1);
+            numbers1[numbersCount1++] = inputValue1;
+            cpt1++;
+
+            if (numbersCount1 >= NUMBERS_COUNT1) {
+                numbersCount1 = 0;
+            }
+            inputText1[0] = '\0';
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE) && strlen(inputText1) > 0) {
+            inputText1[strlen(inputText1) - 1] = '\0';
+        }
+
+        // Ajouter cette section pour créer les rectangles en appuyant sur le bouton Entrer
+        if (IsKeyPressed(KEY_ENTER)) {
+            for (i = 0; i < cpt1; i++) {
+                rectangles[i] = (Rectangle){(i * 70)+400, 350, 70, 70};
+            }
+        }
 
         BeginDrawing();
-             DrawText("TRIE PAR COMPTAGE", windowWidth/2-200, 10, 40, BLUE);
+        DrawText("TRIE PAR COMPTAGE", windowWidth / 2 - 200, 10, 40, BLUE);
+        DrawRectangleLinesEx((Rectangle){10, 10, 160, 770}, 4, BLUE);
+        DrawRectangleLinesEx((Rectangle){300, 150, 650, 500}, 4, BLUE);
 
-        // Draw the border rectangle
-        DrawRectangleLinesEx((Rectangle){ 10, 10, 160, 770 }, 4, BLUE);
+        DrawButtonWithText(buttonBounds1, "Trie", 30, CheckCollisionPointRec(GetMousePosition(), buttonBounds1));
+        DrawButtonWithText(buttonBounds2, "Entre", 30, CheckCollisionPointRec(GetMousePosition(), buttonBounds2));
+        DrawButtonWithText(buttonBounds3, "Exit", 30, CheckCollisionPointRec(GetMousePosition(), buttonBounds3));
 
+        DrawText("N=", windowWidth / 2 - 240, 710, 30, BLACK);
+        DrawRectangleRec(textBox, (mouseOnText && editMode) ? SKYBLUE : LIGHTGRAY);
+        DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, BLUE);
 
-        // Draw the buttons
-        GuiButton(buttonBounds1, "trie");
-        GuiButton(buttonBounds2, "entre");
-        GuiButton(buttonBounds3, "exit");
+        if (strlen(inputText) > 0) {
+            int textWidth = MeasureText(inputText, 30);
+            DrawText(inputText, (int)textBox.x + (int)(textBox.width - textWidth) / 2, (int)textBox.y + (int)(textBox.height - 30) / 2, 30, BLACK);
+        }
 
+        DrawText("T=", windowWidth / 2 - 60, 710, 30, BLACK);
+        DrawRectangleRec(textBox1, (mouseOnText1 && editMode1) ? SKYBLUE : LIGHTGRAY);
+        DrawRectangleLines((int)textBox1.x, (int)textBox1.y, (int)textBox1.width, (int)textBox1.height, BLUE);
 
+        if (strlen(inputText1) > 0) {
+            int textWidth1 = MeasureText(inputText1, 30);
+            DrawText(inputText1, (int)textBox1.x + (int)(textBox1.width - textWidth1) / 2, (int)textBox1.y + (int)(textBox1.height - 30) / 2, 30, BLACK);
+        }
+
+        DrawText("Les elements du tableau:", windowWidth / 2 - 170, 170, 30, BLACK);
+
+        for (int i = 0; i < numbersCount1; i++) {
+            DrawText(TextFormat("%d", numbers1[i]), windowWidth / 2 - 90 + i * 70, 370, 30, BLACK);
+        }
+
+        DrawText(TextFormat("Nombre de saisies: %d", cpt1), windowWidth / 2 - 170, 500, 30, BLACK);
+
+        // Dessiner les rectangles créés en appuyant sur le bouton Entrer
+        for (i = 0; i < cpt1; i++) {
+            DrawRectangleLinesEx(rectangles[i], 4, BLACK);
+        }
 
         EndDrawing();
     }
