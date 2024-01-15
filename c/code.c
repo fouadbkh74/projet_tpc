ea3b4658270afbd67e5af59a1f884070749df050
fouadbkh74 <fouadbkh74@gmail.com>
2024-01-15 13:20:55 +0100

    fin du programme

--- c/code.c
+++ c/code.c
@@ -1,9 +1,15 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define MAX_INPUT_CHARS1 2
 #define NUMBERS_COUNT1 8
+
+typedef struct {
+    int *t;
+    int n;
+} vecteur;
 
 void DrawButtonWithText(Rectangle bounds, const char *text, int fontSize, bool isMouseOnButton) {
     DrawRectangleRec(bounds, isMouseOnButton ? DARKGRAY : LIGHTGRAY);
@@ -14,6 +20,56 @@ void DrawButtonWithText(Rectangle bounds, const char *text, int fontSize, bool i
     Vector2 textPosition = {bounds.x + bounds.width / 2 - textSize.x / 2, bounds.y + bounds.height / 2 - textSize.y / 2};
 
     DrawTextEx(GetFontDefault(), text, textPosition, fontSize, 2, BLUE);
+}
+
+vecteur trie(vecteur v) {
+    int max = v.t[0];
+    vecteur m, r;
+    int i, j;
+
+    // Trouver la valeur maximale dans le vecteur
+    for (i = 1; i < v.n; i++) {
+        if (v.t[i] > max) {
+            max = v.t[i];
+        }
+    }
+
+    // Allocation dynamique du vecteur m.t
+    m.t = (int *)calloc(max + 1, sizeof(int));  // Utilisation de calloc pour initialiser à zéro
+
+    // Compter les occurrences de chaque valeur dans le vecteur v.t
+    for (i = 0; i < v.n; i++) {
+        m.t[v.t[i]]++;
+    }
+
+    // Allocation dynamique du vecteur r.t
+    r.t = (int *)malloc(v.n * sizeof(int));
+
+    // Remplir le vecteur trié r.t en utilisant le tri par comptage
+    int k = 0;
+    for (i = 0; i <= max; i++) {
+        for (j = 0; j < m.t[i]; j++) {
+            r.t[k++] = i;
+        }
+    }
+
+    // Libérer la mémoire allouée pour le vecteur m.t
+    free(m.t);
+
+    // Ajuster la taille de r
+    r.n = v.n;
+
+    return r;
+}
+
+// la fonction qui affiche le vecteur
+void affich(vecteur v) {
+    int i;
+    printf("Affichage du vecteur trié : \n");
+
+    for (i = 0; i < v.n; i++) {
+        printf("v.t[%d] = %d\n", i, v.t[i]);
+    }
 }
 
 int main(void) {
@@ -33,7 +89,9 @@ int main(void) {
     char inputText[3] = "\0"; // Pour un entier décimal
     int i;
 
-    ///////////////////////////////////////
+    vecteur v, y;
+    int  l;
+
     Rectangle textBox1 = {windowWidth / 2 - 10, 700, 400, 50};
     bool mouseOnText1 = false;
     bool editMode1 = false;
@@ -42,6 +100,12 @@ int main(void) {
     int numbers1[NUMBERS_COUNT1];  // Tableau pour stocker les nombres saisis
     int numbersCount1 = 0;
     int cpt1 = 0; // Variable pour compter le nombre de saisies
+
+
+    SetExitKey(KEY_NULL);       // Disable KEY_ESCAPE to close window, X-button still works
+
+    bool exitWindowRequested = false;   // Flag to request window to exit
+    bool exitWindow = false;    // Flag to set window to exit
 
     while (!WindowShouldClose()) {
         ClearBackground(WHITE);
@@ -80,7 +144,7 @@ int main(void) {
 
         if (IsKeyPressed(KEY_ENTER) && strlen(inputText) > 0) {
             int inputValue = atoi(inputText);
-            if (inputValue >= 0 && inputValue <= 99) {
+            if (inputValue >= 0 && inputValue <= 1) {
                 printf("Valeur saisie : %d\n", inputValue);
 
             } else {
@@ -93,22 +157,26 @@ int main(void) {
             inputText[strlen(inputText) - 1] = '\0';
         }
 
-        // Gestion de la deuxième boîte de texte
+        // Gestionde la deuxième boîte de texte
         if (editMode1) {
             int key1 = GetKeyPressed();
             if (key1 > 0) {
                 if (isdigit(key1) && strlen(inputText1) < MAX_INPUT_CHARS1) {
                    strcat(inputText1, (char[]){(char)key1, '\0'});
+
                 } else if (key1 == ',') {
                     int inputValue1 = atoi(inputText1);
                     numbers1[numbersCount1++] = inputValue1;
+                    v.t[i]= inputValue1;
                     cpt1++;
-                    if (numbersCount1 >= NUMBERS_COUNT1) {
-                        numbersCount1 = 0;
-                    }
-                    inputText1[0] = '\0';
+
                 }
             }
+        v.t=(int*)malloc(sizeof(int)*cpt1);
+        for(i=0;i<v.n;i++){
+
+            v.t[i]=numbers1[i];
+        }
         }
 
         if (IsKeyPressed(KEY_ENTER) && strlen(inputText1) > 0) {
@@ -121,6 +189,7 @@ int main(void) {
             }
             inputText1[0] = '\0';
         }
+        v.n=cpt1;
 
         if (IsKeyPressed(KEY_BACKSPACE) && strlen(inputText1) > 0) {
             inputText1[strlen(inputText1) - 1] = '\0';
@@ -133,49 +202,33 @@ int main(void) {
             }
         }
 
-        if (editMode && key > 0) {
-            // Accepter uniquement des chiffres pour l'entrée décimale
-            if (isdigit(key) && strlen(inputText) < 2) {
-                strcat(inputText, (char[]){(char)key, '\0'}); // Ajouter le caractère à la fin de la chaîne
-            }
+        // Gestion du bouton Entre
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), buttonBounds1)) {
+            y = trie(v);
+            affich(y);
+
+            // Libérer la mémoire allouée pour les vecteurs v.t et y.t
+            free(v.t);
+            free(y.t);
+
+            printf("Bouton Entre appuyé!\n");
         }
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), buttonBounds3)) {
 
-        if (IsKeyPressed(KEY_ENTER) && strlen(inputText) > 0) {
-            // Convertir la chaîne de caractères en une valeur entière décimale
-            int inputValue = atoi(inputText);
+        // Update
+        //----------------------------------------------------------------------------------
+        // Detect if X-button or KEY_ESCAPE have been pressed to close window
+        exitWindowRequested = true;}
 
-            // Vérifier si la taille du nombre ne dépasse pas 2 chiffres
-            if (inputValue >= 0 && inputValue <= 99) {
-                // Faites quelque chose avec la valeur entière (inputValue)
-                // Par exemple, imprimez-la :
-                printf("Valeur saisie : %d\n", inputValue);
-            } else {
-                // La valeur n'est pas dans la plage spécifiée, gérer l'erreur
-                printf("Erreur : La valeur doit être entre 0 et 99 inclus.\n");
-            }
+        if (exitWindowRequested)
+        {
+            // A request for close window has been issued, we can save data before closing
+            // or just show a message asking for confirmation
 
-            // Réinitialiser la boîte de saisie
-            inputText[0] = '\0';
+            if (IsKeyPressed(KEY_Y))     CloseWindow();
+
+            else if (IsKeyPressed(KEY_N)) exitWindowRequested = false;
         }
-
-        if (IsKeyPressed(KEY_BACKSPACE) && strlen(inputText) > 0) {
-            inputText[strlen(inputText) - 1] = '\0'; // Retirer le dernier caractère
-        } else {
-            SetMouseCursor(editMode ? MOUSE_CURSOR_IBEAM : MOUSE_CURSOR_DEFAULT);
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         BeginDrawing();
         DrawText("TRIE PAR COMPTAGE", windowWidth / 2 - 200, 10, 40, BLUE);
@@ -190,7 +243,6 @@ int main(void) {
         DrawRectangleRec(textBox, (mouseOnText && editMode) ? SKYBLUE : LIGHTGRAY);
         DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, BLUE);
 
-<<<<<<< HEAD
         if (strlen(inputText) > 0) {
             int textWidth = MeasureText(inputText, 30);
             DrawText(inputText, (int)textBox.x + (int)(textBox.width - textWidth) / 2, (int)textBox.y + (int)(textBox.height - 30) / 2, 30, BLACK);
@@ -217,31 +269,13 @@ int main(void) {
         for (i = 0; i < cpt1; i++) {
             DrawRectangleLinesEx(rectangles[i], 4, BLACK);
         }
-=======
-        // Draw the green background rectangle
-        DrawRectangleLinesEx((Rectangle){300, 150, 500, 500}, 4, BLUE);
+if (exitWindowRequested)
+            {
+                DrawRectangle(0, 200, windowWidth, 200, BLACK);
+                DrawText("Are you sure you want to exit program? [Y/N]", 40, 280, 30, WHITE);
+            }
+            //else DrawText("Try to close the window to get confirmation message!", 120, 200, 20, LIGHTGRAY);
 
-        // Draw the buttons
-        GuiButton(buttonBounds1, "trie");
-        GuiButton(buttonBounds2, "entre");
-        GuiButton(buttonBounds3, "exit");
-
-        { ////// textbox n=
-            // Dessiner la phrase "N=" avant le textBox
-        DrawText("N=", windowWidth / 2 - 240, 710, 30, BLACK);
-
-        DrawRectangleRec(textBox, (mouseOnText && editMode) ? SKYBLUE : LIGHTGRAY);
-        DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, BLUE); // BORDER
-
-        // Dessiner le texte uniquement s'il y a du texte dans la boîte
-        if (strlen(inputText) > 0) {
-            // Mesurer la largeur du texte affiché
-            int textWidth = MeasureText(inputText, 30);
-
-            // Centrer le texte dans la boîte de texte
-            DrawText(inputText, (int)textBox.x + (int)(textBox.width - textWidth) / 2, (int)textBox.y + (int)(textBox.height - 30) / 2, 30, BLACK);
-        }}
->>>>>>> 6b58033822ad8ab1cb97c0f282bce6ca4e3b632c
 
         EndDrawing();
     }
